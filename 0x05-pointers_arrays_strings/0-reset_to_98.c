bd51f31c2c14c6a9610e381c7220bc4c6453d2fd
BlessyErn <betnyabereka@gmail.com>
2022-07-11 18:16:12 -0700

    Task 0 Hot fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *  reset_to_98 - resets the valie of the variable.
@@ -8,7 +9,7 @@
  *  Rerurn: return void.
  */
 
-reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
