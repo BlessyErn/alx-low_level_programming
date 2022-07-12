af78b1e2a603d73208eb98b04d006afb6c389f8b
BlessyErn <betnyabereka@gmail.com>
2022-07-11 19:48:44 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+ /**
+  * print_rev - print a steing in reverse
+  * @s: argument to the function
+  * Description: return the required result
+  * Return: retuen void
+  */
+
+void print_rev(chaf *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (length = length - 1; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
