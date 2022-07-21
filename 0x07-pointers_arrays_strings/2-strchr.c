9aaeb457c12955a1d0365a5aca0c1a5a6a41e5c9
BlessyErn <106969332+BlessyErn@users.noreply.github.com>
2022-07-21 15:35:04 +0200

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strchr - locate character in string
- * @s: source string
- * @c: character to find
- * Return: the string from character found
- */
-
-char *_strchr(char *s, char c);
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
 {
 	int a = 0, b;
 
@@ -24,5 +24,6 @@ char *_strchr(char *s, char c);
 			return (s);
 		}
 	}
+
 	return ('\0');
 }
