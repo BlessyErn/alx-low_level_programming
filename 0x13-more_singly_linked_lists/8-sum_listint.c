9b8f3fb088a23abb1aab3a0048d9b7e5e7b93b20
BlessyErn <106969332+BlessyErn@users.noreply.github.com>
2022-08-26 12:54:41 +0200

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,7 +5,6 @@
  * Description: sum of all the data (n) of a listint_t linked list.
  *
  * @head: pointer of type listint_t
- *
  * Return: returns sum, or 0 if empty
  */
 int sum_listint(listint_t *head)
